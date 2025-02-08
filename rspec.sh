da8b7df639133baba5f4697ff7fbff044512eab8
Bergson Lima <bergsonsud@gmail.com>
2025-02-08 12:47:26 -0300

    Adiciona swagger

--- rspec.sh
+++ rspec.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker compose run --service-ports --rm -e "RAILS_ENV=test" api bin/rails db:create db:migrate
-docker compose run --service-ports --rm -e "RAILS_ENV=test" api bundle exec rspec
+docker compose run --service-ports --rm -e "RAILS_ENV=test" api bundle exec rspec --exclude-pattern "spec/integration/**/*_spec.rb"
